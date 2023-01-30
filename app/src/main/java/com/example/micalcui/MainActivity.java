1f998eed772498bb8901ace93061561eefc679ad
Narendra <narendra766102@gmail.com>
2023-01-30 14:57:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/micalcui/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.micalcui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView result_tv,solution_tv;
+    MaterialButton buttonC,buttonOpenBracl,buttonCloseBrac;
+    MaterialButton buttonDivide,buttonMultiply,buttonAdd,buttonSub,buttonEqals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    MaterialButton buttonAc,buttonDot;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        result_tv=findViewById(R.id.result_tv);
+        solution_tv=findViewById(R.id.solution_tv);
+        assignId(buttonC,R.id.button_c);
+
+        assignId(buttonDivide,R.id.button_divide);
+        assignId(buttonMultiply,R.id.button_multiply);
+        assignId(buttonAdd,R.id.button_plus);
+        assignId(buttonSub,R.id.button_minus);
+        assignId(buttonEqals,R.id.equal);
+        assignId(button0,R.id.button_0);
+        assignId(button1,R.id.button_1);
+        assignId(button2,R.id.button_2);
+        assignId(button3,R.id.button_3);
+        assignId(button4,R.id.button_4);
+        assignId(button5,R.id.button_5);
+        assignId(button6,R.id.button_6);
+        assignId(button7,R.id.button_7);
+        assignId(button8,R.id.button_8);
+        assignId(button9,R.id.button_9);
+        assignId(buttonAc,R.id.button_AC);
+        assignId(buttonDot,R.id.button_dot);
+
+    }
+    void assignId( MaterialButton btn,int id){
+        btn=findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText=button.getText().toString();
+        String dataToCalculate = solution_tv.getText().toString();
+
+        if(buttonText.equals("AC"))
+        {
+            solution_tv.setText("");
+            result_tv.setText("0");
+            return;
+        }
+        if(buttonText.equals("="))
+        {
+            solution_tv.setText(result_tv.getText());
+            return;
+        }
+        if (buttonText.equals("C"))
+        {
+            dataToCalculate=dataToCalculate.substring(0,dataToCalculate.length()-1);
+
+        }else {
+        dataToCalculate=dataToCalculate+buttonText;
+        }
+        solution_tv.setText(dataToCalculate);
+        String finalResult=getResult(dataToCalculate);
+        if(!finalResult.equals("Err"))
+        {
+            result_tv.setText(finalResult);
+        }
+    }
+
+    String getResult(String data)
+    {
+        try{
+            Context context=Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable=context.initStandardObjects();
+           String finalResult= context.evaluateString(scriptable,data,"Javascript",1,null).toString();
+
+           if(finalResult.endsWith(".0")){
+               finalResult=finalResult.replace(".0","");
+           }
+           return finalResult;
+        }
+        catch (Exception e){
+            return "Err";
+        }
+    }
+}
